096c7ccfce81b80154f92cd07c65bb7a313aa06a
Kaylee Xu <kaylee@Kaylees-MacBook-Air.local>
2026-01-26 22:51:26 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -18,3 +18,25 @@ print(math.pow(2, 3))
 print(math.sqrt(2)) # square root of x
 print(math.log(2)) # x in log base e
 
+# hypotenuse c given sides a and b
+a = 3	# side of triangle
+b = 4 	# side of triangle
+c = math.sqrt(a**2 + b**2) 	# hypotenuse
+print(c)
+
+print(type(a), type(b), type(c))
+print(type(a), type(b), type(c), sep=', ', end='!\n')
+# \n means "newline"
+
+def pythagoras(a, b):
+	c = math.sqrt(a**2 + b**2)
+	return c
+# a and b are "parameters" or "arguments" of the function
+
+hyp = pythagoras (3, 4)
+print(hyp)
+
+hyp = pythagoras(3, 4)
+	return math.sqrt(a**2 + b**2)
+	
+print(pythagoras(3, 4))
