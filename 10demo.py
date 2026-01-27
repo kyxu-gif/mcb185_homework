f2e268a43dd875f01d624b31e666d770655e036c
Kaylee Xu <kaylee@Kaylees-MacBook-Air.local>
2026-01-26 22:18:28 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,20 @@
+# 10demo.py by kaylee_xu
+
+print("hello, again") # greeting
+
+print(1.5e-2) # scientific notation
+
+print(1 + 1) # addition
+print(1 - 1) # subtraction
+print(2 * 2) # multiplication
+print(1 / 2) # division
+print(2 ** 3) # exponentiation
+print(5 // 2) # integer divide
+print(5 % 2) # remainder
+print (5 * (2 + 1)) # precedence
+
+print(pow(2, 3)) # x to power of y
+print(math.pow(2, 3))
+print(math.sqrt(2)) # square root of x
+print(math.log(2)) # x in log base e
+
